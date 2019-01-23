df30d3f69970ea2cc53bbfe0716898a5f7896d56
Tommy <tommyhta@gmail.com>
2019-01-23 12:36:04 -0800

    cleaned up code

--- Java/src/me/com/selectionSort/Main.java
+++ Java/src/me/com/selectionSort/Main.java
@@ -5,25 +5,32 @@ public class Main {
 //for this implementation, we will find the largest number and send it to the back of the array
 
     public static void main(String[] args) {
-	    int[] arr = {7,3,6,10,2,5,1,0,4};
+	    int[] uAr = {7,3,6,10,2,5,1,0,4};
 
-	    //the for loop will starts at the highest index and works its way to 0
-	    for(int i = arr.length-1; i>0 ; i--){
-	        int max = 0;
-	        for (int j = 1; j<=i; j++) {
-                if(arr[j]>arr[max]){
-                    max = j;
-                }
-            }
-	        //only when the index of the highest number is found, will the swap happens
-	        int temp = arr[i];
-	        arr[i]=arr[max];
-	        arr[max] = temp;
+	    cLog(selectionSort(uAr));
 
-        }
-
-	    for(int i =0; i<arr.length; i++){
-	        System.out.println(arr[i]);
-        }
     }
+
+    public static int[] selectionSort(int[] arr){
+		//the for loop will starts at the highest index and works its way to 0
+		for(int i = arr.length-1; i>0 ; i--){
+			int max = 0;
+			for (int j = 1; j<=i; j++) {
+				if(arr[j]>arr[max]){
+					max = j;
+				}
+			}
+			//only when the index of the highest number is found, will the swap happens
+			int temp = arr[i];
+			arr[i]=arr[max];
+			arr[max] = temp;
+		}
+    	return arr;
+	}
+
+	public static void cLog(int[] arr){
+		for(int i =0; i<arr.length; i++){
+			System.out.println(arr[i]);
+		}
+	}
 }
